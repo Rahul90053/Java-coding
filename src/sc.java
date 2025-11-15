626d558adb858095a9dcebeea9fddf417d9c2b49
Rahul90053 <jha702251@gmail.com>
2025-11-15 11:06:23 +0530

    git commit

---
+++ src/sc.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sc {
+    static void main() {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the name:");
+        String name = sc.next();
+        System.out.println("Enter your gender:");
+        char gender= sc.next().charAt(0);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Enter your Phone.no");
+        long phone = sc.nextLong();
+
+    }
+}
