6d831fa6dad0fe1ac8667e8c632ba7059bd186ec
Rahul90053 <jha702251@gmail.com>
2025-11-15 10:11:50 +0530

    git commit

--- src/leap.java
+++ src/leap.java
@@ -1,2 +1,18 @@
 public class leap {
+    public static void main(String args[]){
+        int year =2016;
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    System.out.println("leap year");
+                }else{
+                    System.out.println("Not leap year");
+                }
+            }else{
+                System.out.println("leap year");
+            }
+        } else{
+            System.out.println("Not a leap year");
+        }
+    }
 }
